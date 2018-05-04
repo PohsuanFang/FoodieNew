0cbeecb6b53d6b1b3cadd4e42378bca4ec898225
PohsuanFang <36302986+PohsuanFang@users.noreply.github.com>
2018-05-04 09:32:53 -0400

    Add files via upload

---
+++ coffeeshops.js
@@ -0,0 +1,29 @@
+// //find out the names of the coffeeshops that are 1 miles away from MIT
+
+var coffeeList=[];
+var loaddata=function(){
+    // console.log("loading datattttttt");
+
+
+    coffee.businesses.forEach(function(item)
+    {
+        var coffeeInfo={};
+        coffeeInfo.name=item["name"];
+        // console.log("\n  coffeeInfo.name= "+ coffeeInfo.name);
+        
+        coffeeInfo.lat=item["coordinates"]["latitude"];
+        // console.log("\n     coffeeInfo.lat = "+coffeeInfo.lat);
+        coffeeInfo.lng=item["coordinates"]["longitude"];
+        coffeeList.push(coffeeInfo);
+    }
+    );
+    
+    // console.log("coffelist "+coffeeList);
+
+};
+
+
+
+
+
+
